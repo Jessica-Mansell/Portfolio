a390710a18e64e9fc76935c8b2476bf0bab8526b
Jessica-Mansell <jcmansell88@gmail.com>
2022-10-31 15:36:56 -0500

    add to path for template render

--- django-port/projects/views.py
+++ django-port/projects/views.py
@@ -7,5 +7,5 @@ from django.shortcuts import render
 def all_projects(request):
     # query the db to return all project objects
     projects = Project.objects.all()
-    return render(request, 'all_projects.html', 
+    return render(request, 'projects/all_projects.html', 
                     {'projects': projects})
