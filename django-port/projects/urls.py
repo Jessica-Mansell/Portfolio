4ad003622dcf9c4690c70830e1dbaab8c81dd283
Jessica-Mansell <jcmansell88@gmail.com>
2022-11-17 13:04:15 -0600

    add index url for home page

--- django-port/projects/urls.py
+++ django-port/projects/urls.py
@@ -4,6 +4,7 @@ from projects import views
 app_name = 'projects'
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path('', views.all_projects, name='all_projects'),
     path('<int:pk>', views.project_detail, name='project_detail'),
 ]
