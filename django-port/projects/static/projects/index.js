f1a3a10807bf6d73fa013206611d14668d61ec8c
Jessica-Mansell <jcmansell88@gmail.com>
2022-12-10 23:11:16 -0600

    add TODOs

--- django-port/projects/static/projects/index.js
+++ django-port/projects/static/projects/index.js
@@ -10,19 +10,24 @@ function load_home() {
 };
 
 // TODO: Write function for single page view detail for each project page
-function show_project(project) {
-    fetch(`/projects/${project}`)
-    .then(response => response.text())
-    .then(text => {
-        console.log(text);
-        document.querySelector('#project_view').innerHTML = text;
-    });
-}
+// UPDATE: The function is written however, I need to wait until the project_view is
+// implemented on the all_projects.html page instead of the detail.html
+// function show_project(project) {
+//     fetch(`/projects/${project}`)
+//     .then(response => response.text())
+//     .then(text => {
+//         console.log(text);
+//         document.querySelector('#project_view').innerHTML = text;
+//     });
+// }
+// TODO: Button changes will require the querySelector to switch which button
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.querySelector('button_more').forEach(button => {
+//         button.onclick = function() {
+//             show_project(this.dataset.project)
+//         }
+//     })
+// })
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('button_more').forEach(button => {
-        button.onclick = function() {
-            show_project(this.dataset.project)
-        }
-    })
-})
+// ***Note: Refer to load_mailbox function in mail app for idea on how to implement all_projects view
+// ***Note: To replace detail.html webpage, refer to the view_email function in mail app
